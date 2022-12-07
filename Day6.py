747da421f66017440fe2775171f57c416b2b9d85
Mykyta <nikita.syd08@gmail.com>
2022-12-07 17:48:40 +0100

    Day 6

---
+++ Day6.py
@@ -0,0 +1,31 @@
+import os
+from collections import deque
+from typing import List
+
+cur_dir = os.getcwd()
+path = cur_dir + '/AdventCode/input6.txt'
+
+
+with open(path, 'r') as file:
+    counter = 0
+    for f in file:
+        array = f
+
+# For part 2, change 4 to 14 at every encounter below
+window = {}
+for i, c in enumerate(array):
+    if i < 4:
+        window[c] = window.get(c, 0) + 1
+        continue
+
+    if len(window) == 4:
+        index = i
+        break
+    
+    window[c] = window.get(c, 0) + 1
+    remove_c = array[i - 4]
+    window[remove_c] -= 1
+    if window[remove_c] == 0: window.pop(remove_c)
+
+
+print(index)
